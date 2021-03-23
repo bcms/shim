02eb776a0e147747ecfa6e795221fb31a6ff77c4
Bbanez <banezprod@gmail.com>
2021-03-23 16:43:45 +0100

    Development

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -9,7 +9,7 @@ import {
   Post,
 } from '@becomes/purple-cheetah';
 import { ConnectionService } from '../services';
-import type { InstanceUser } from '../types';
+import type { ShimInstanceUser } from '../types';
 
 @Controller('/instance/user')
 export class ShimInstanceUserController
@@ -20,32 +20,12 @@ export class ShimInstanceUserController
   baseUri: string;
   initRouter: () => void;
 
-  @Post('/verify')
-  async verify(
-    request: Request,
-  ): Promise<{
-    ok: boolean;
-    user?: InstanceUser;
-  }> {
-    const error = HttpErrorFactory.instance('verify', this.logger);
-    const instanceId = request.headers['bcms-iid'] as string;
-    return await ConnectionService.send(
-      instanceId,
-      '/user/verify',
-      {
-        email: request.body.email,
-        password: request.body.password,
-      },
-      error,
-    );
-  }
-
   @Post('/verify/otp')
   async verifyWithOTP(
     request: Request,
   ): Promise<{
     ok: boolean;
-    user?: InstanceUser;
+    user?: ShimInstanceUser;
   }> {
     const error = HttpErrorFactory.instance('verifyWithOTP', this.logger);
     const instanceId = request.headers['bcms-iid'] as string;
@@ -60,7 +40,7 @@ export class ShimInstanceUserController
   }
 
   @Post('/all')
-  async getAll(request: Request): Promise<{ user: InstanceUser[] }> {
+  async getAll(request: Request): Promise<{ user: ShimInstanceUser[] }> {
     const error = HttpErrorFactory.instance('getAll', this.logger);
     const instanceId = request.headers['bcms-iid'] as string;
     return await ConnectionService.send(
--- src/types/cloud/user.ts
+++ src/types/cloud/user.ts
@@ -1,40 +1,19 @@
-import type { RoleName } from '@becomes/purple-cheetah';
+import type { Role, RoleName } from '@becomes/purple-cheetah';
 
-export interface InstanceUserOrg {
+export interface ShimInstanceUserOrg {
   id: string;
   role: RoleName;
   owner: boolean;
 }
 
-export interface InstanceUser {
+export interface ShimInstanceUser {
   _id: string;
   createdAt: number;
   updatedAt: number;
   username: string;
-  organization: InstanceUserOrg[];
+  firstName: string;
+  lastName: string;
+  email: string;
+  organizations: ShimInstanceUserOrg[];
+  roles: Role[];
 }
-
-// import type { Role, RoleName } from '@becomes/purple-cheetah';
-
-// export interface UserPersonal {
-//   firstName: string;
-//   lastName: string;
-//   avatarUri: string;
-// }
-// export interface UserCustomPool {
-//   personal: UserPersonal;
-//   organizations: Array<{
-//     id: string;
-//     role: RoleName;
-//     owner: boolean;
-//   }>;
-// }
-// export interface UserProtected {
-//   _id: string;
-//   createdAt: number;
-//   updatedAt: number;
-//   username: string;
-//   email: string;
-//   roles: Role[];
-//   customPool: UserCustomPool;
-// }
